784821655586a840973b47dcff7d5f1fbe3adab5
fortdev210 <sharp.blader2050@gmail.com>
2023-04-12 04:11:18 +0800

    add posts

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Local
-    "accounts.apps.AccountsConfig"
+    "accounts.apps.AccountsConfig",
+    "posts.apps.PostsConfig"
 ]
 
 MIDDLEWARE = [

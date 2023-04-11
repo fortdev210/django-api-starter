1be27ce34afdb842369377999c721d519139ff13
fortdev210 <sharp.blader2050@gmail.com>
2023-04-12 04:37:47 +0800

    add secret key and unit test

--- main/settings.py
+++ main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+#mio-!$q$y*3_q!%^+ufi7wx@3_nng#_h!a(ag3yv)knevl!4'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser",
+            email="test@gmail.com",
+            password="password"
+        )
+
+        cls.post = Post.objects.create(
+            author=cls.user,
+            title="A good title",
+            body="Good post description"
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(self.post.title, "A good title")
